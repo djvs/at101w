24cfea89e4152f757e80a65e67ede4c4dffc1806
djvs <djvs@users.noreply.github.com>
2023-01-23 18:52:13 -0500

    updates

--- outputs/gerber/jlcpcb_cpl.sh
+++ outputs/gerber/jlcpcb_cpl.sh
@@ -7,7 +7,9 @@ perl -pi -e "s/Ref,Val,Package,PosX,PosY,Rot,Side/Designator,Mid X,Mid Y,Rotatio
 perl -pi -e 's/","[^"]*","[^"]*"/"/' jlcpcb-cpl*.csv
 
 # manual placement fix
-perl -pi -e 's/^"U3",(.*),180/"U3",\1,-180/' jlcpcb-cpl*.csv
-perl -pi -e 's/^"U4",(.*),-90/"U4",\1,0/' jlcpcb-cpl*.csv
-perl -pi -e 's/^"J2",(.*),0.0000/"J2",\180.0000/' jlcpcb-cpl*.csv
-perl -pi -e 's/^"Y1",(.*),0.0000/"Y1",\1,180.0000/' jlcpcb-cpl*.csv
+perl -pi -e 's/^"U2",(.*),-90/"U2",\1,90/' jlcpcb-cpl*.csv
+perl -pi -e 's/^"U3",(.*),180/"U3",\1,-90/' jlcpcb-cpl*.csv
+perl -pi -e 's/^"U4",(.*),-90/"U4",\1,180/' jlcpcb-cpl*.csv
+perl -pi -e 's/^"J2",(.*),0.000000,bottom/"J2",\1,180.000000,bottom/' jlcpcb-cpl*.csv
+perl -pi -e 's/^"Y1",(.*),0.000000/"Y1",\1,180.000000/' jlcpcb-cpl*.csv
+perl -pi -e 's/^"U6",(.*),-90/"U6",\1,90/' jlcpcb-cpl*.csv
